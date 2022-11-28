c1ff29c624cbc32a6d116e4033e003ed3e642e0a
= <=>
2022-11-28 20:03:41 +0100

    Fixed edge cases where update value has spaces

--- console.py
+++ console.py
@@ -58,38 +58,6 @@ def parser(line):
             quoted_str_index_counter += 1
     return argv
 
-# This version of parser is used only in precmd
-# It returns argv in which the quoted strings still have their quotes
-
-
-def parser2(line):
-    """Parses arguments parsed to our HBNBCommands
-    Returns an array of strings
-
-    Args:
-        line(str): String passed to cmd
-    """
-
-    # Regex gets all sub strings within quotation marks and
-    # stores it an av_quoted array in order to treat is as a unit
-    av_quoted = re.findall(r'(["\'][^"\']+["\'])', line)
-
-    # Each quoted sub string is replaced in line with
-    # a place holder "quoted_str"
-    line = re.sub(r'(["\'][^"\']+["\'])', "quoted_str", line)
-
-    # The regex returns a list of all space-demarcated substrings
-    argv = re.findall(r"(\b[^\s]+\b)", line)
-
-    quoted_str_index_counter = 0
-    # The placeholder "quoted_str" is replaced with the original
-    # quoted string
-    for idx, arg in enumerate(argv):
-        if arg == "quoted_str":
-            argv[idx] = av_quoted[quoted_str_index_counter]
-            quoted_str_index_counter += 1
-    return argv
-
 
 def verify_id(id, argv):
     """Verifies if given id exists and returns True when it does
@@ -112,44 +80,35 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def precmd(self, line):
-        """Overwrites the onecmd. This helps us to rearrange
-        the arguments in the format of class.cmd() to fit into
-        already existing do_foo methods in onecmd
-        """
+        if '.' in line:
+            cls, comd = line.split('.')
+            comd = comd.split('(')[0]
 
-        # Contains list of all commands that require id
-        cmd_list = ["update", "destroy", "show"]
-
-        # checks for arguments in the format of class.cmd()
-        if "." in line:
-            argv = line.split('.')
-            # Regex returns the command (comd) without the ending ()
-            comd = re.findall(r"^[a-z]+", argv[1])[0]
-            if comd in cmd_list:
-                # Gets the arg passed to bracket
-                arg_in_bracket = re.findall(r"\((.*)\)", argv[1])[0]
-                if comd == "update":
-                    # av is a list of commands passed to update method
-                    av = parser2(arg_in_bracket)
-                    if len(av) == 3:
-                        # Checks value parameter is passed as a dict
-                        if type(av[2]) is dict:
-                            for k, v in av[2].items():
-                                av[1] = k
-                                av[2] = v
-                            line = f'{comd} {av[0]} {av[0]} {av[1]} {av[2]}'
-                        line = f'{comd} {argv[0]} {av[0]} {av[1]} {av[2]}'
-                    elif len(av) == 2:
-                        line = f"{comd} {argv[0]} {av[0]} {av[1]}"
-                    elif len(av) == 1:
-                        line = f"{comd} {argv[0]} {av[0]}"
-                else:
-                    # Reconstructs the line to fit cmd class pattern
-                    line = f"{comd} {argv[0]} {arg_in_bracket}"
-            else:
-                line = f"{comd} {argv[0]}"
-
-        # Returns the line as an argument to one cmd
+            # Gets the arg inside bracket
+            cmd_args = line.split('(')[1]
+            if '{' not in cmd_args:
+                cmd_args = cmd_args.split(', ')
+                for idx, arg in enumerate(cmd_args):
+                    cmd_args[idx] = arg.strip(')')
+                    if len(cmd_args) == 0:
+                        line = f"{comd} {cls}"
+                    elif len(cmd_args) >= 1:
+                        id = cmd_args[0]
+                        if len(cmd_args) == 1:
+                            line = f"{comd} {cls} {id}"
+                        elif len(cmd_args) == 2:
+                            attr_name = cmd_args[1]
+                            line = f'{comd} {cls} {id} {attr_name}'
+                        elif len(cmd_args) == 3:
+                            attr_name = cmd_args[1]
+                            value = cmd_args[2]
+                            line = f'{comd} {cls} {id} {attr_name} {value}'
+            elif '{' in cmd_args:
+                cmd_args = list(re.findall(
+                    r'(["\'].+["\']), (\{.*\})', cmd_args)[0])
+                dict_args = eval(cmd_args[1])
+                id = cmd_args[0]
+                line = f"{comd} {cls} {id} {dict_args}"
         return cmd.Cmd.precmd(self, line)
 
     def do_EOF(self, line):
@@ -254,43 +213,59 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, line):
         """Updates an instance based on the class and id by adding attribute
         """
-        argv = parser(line)
-        if len(argv) == 0:
-            print("** class name missing **")
-        elif argv[0] not in CLASSES:
-            print("** class doesn't exist **")
-        elif len(argv) == 1:
-            print("** instance id missing **")
-        elif len(argv) == 2:
-            if verify_id(argv[1], argv) is False:
-                print("** no instance found **")
-            else:
-                print("** attribute name missing **")
+        # A list of all lines to be updated
+        all_lines = []
 
-        elif len(argv) == 3:
-            if verify_id(argv[1], argv) is False:
-                print("** no instance found **")
-            else:
-                print("** value missing **")
+        if '{' in line:
+            dict_args = re.findall(r'(\{.*\})', line)[0]
+            line = re.sub(r'(\{.*\})', "dict", line)
+            dict_args = eval(dict_args)
+            argv = parser(line)
+            for k, v in dict_args.items():
+                line = f'{argv[0]} "{argv[1]}" "{k}" "{v}"'
+                all_lines.append(line)
         else:
-            if verify_id(argv[1], argv) is False:
-                print("** no instance found **")
-            else:
-                all_dict = storage.all()
-                key = f"{argv[0]}.{argv[1]}"
-                obj = all_dict.get(key)
+            all_lines.append(line)
 
-                # if attribute name already exist, verify its type
-                # and cast new value to the existing type
-                if argv[2] in type(obj).__dict__:
-                    attr_type = type(obj.__class__.__dict__[argv[2]])
-                    setattr(obj, argv[2], attr_type(argv[3]))
+        # A loop to update every line in all_lines
+        for line in all_lines:
+            argv = parser(line)
+            if len(argv) == 0:
+                print("** class name missing **")
+            elif argv[0] not in CLASSES:
+                print("** class doesn't exist **")
+            elif len(argv) == 1:
+                print("** instance id missing **")
+            elif len(argv) == 2:
+                if verify_id(argv[1], argv) is False:
+                    print("** no instance found **")
                 else:
-                    # if attribute does not exist, just assign to value
-                    setattr(obj, argv[2], argv[3])
+                    print("** attribute name missing **")
 
-                # save the updated objects
-        storage.save()
+            elif len(argv) == 3:
+                if verify_id(argv[1], argv) is False:
+                    print("** no instance found **")
+                else:
+                    print("** value missing **")
+            else:
+                if verify_id(argv[1], argv) is False:
+                    print("** no instance found **")
+                else:
+                    all_dict = storage.all()
+                    key = f"{argv[0]}.{argv[1]}"
+                    obj = all_dict.get(key)
+
+                    # if attribute name already exist, verify its type
+                    # and cast new value to the existing type
+                    if argv[2] in type(obj).__dict__:
+                        attr_type = type(obj.__class__.__dict__[argv[2]])
+                        setattr(obj, argv[2], attr_type(argv[3]))
+                    else:
+                        # if attribute does not exist, just assign to value
+                        setattr(obj, argv[2], argv[3])
+
+                    # save the updated objects
+            storage.save()
 
     def do_count(self, line):
         """Retrives the count of instances of a class
